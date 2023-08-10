6cd9c23caf3929446fab05d572251bf581e6dd67
residriza <rexidriza1208@gmail.com>
2023-08-10 17:54:36 +0300

    first commit

---
+++ Ex0.java
@@ -0,0 +1,14 @@
+
+
+
+
+/**
+ *
+ * @author Matros
+ */
+public class Ex0 {
+
+    public static void main(String[] args) {
+        System.out.println("Tantuni ayran kalbim sana hayran");
+    }
+}
